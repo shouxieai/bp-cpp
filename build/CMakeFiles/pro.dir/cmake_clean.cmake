1c8ecd020a92520a7a617a0d91944ec10f0865d1
Wish <dujw@deepblueai.com>
2021-08-11 19:13:47 +0800

    add cmake

---
+++ build/CMakeFiles/pro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pro.dir/src/main.cpp.o"
+  "../workspace/pro.pdb"
+  "../workspace/pro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/train.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
