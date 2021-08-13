b79f77f0f9c14b52dab229ec21ccdc754a715dcc
Wish <dujw@deepblueai.com>
2021-08-13 20:41:00 +0800

    up

--- build/CMakeFiles/dotest.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dotest"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dotest.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/train.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/train"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/train.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
