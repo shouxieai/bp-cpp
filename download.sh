f6d3d60c68a2f5b6c3f58c2bffef078e618917c7
手写AI <87959426+shouxieai@users.noreply.github.com>
2022-04-02 22:12:29 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download dataset
-wget http://www.zifuture.com:8090/upload/2021/08/mnist.dataset-a2fb0c8f1f91477d871f822419128672.tar.gz --output-document=mnist.dataset.tar.gz
+wget http://www.zifuture.com:1556/fs/upload/2021/08/mnist.dataset-a2fb0c8f1f91477d871f822419128672.tar.gz --output-document=mnist.dataset.tar.gz
 tar -zxvf mnist.dataset.tar.gz
 rm -f mnist.dataset.tar.gz
 
