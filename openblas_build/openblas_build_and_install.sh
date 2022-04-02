c73587dfb0ca5d11e7ee6912092048122817f4ed
手写AI <87959426+shouxieai@users.noreply.github.com>
2022-04-02 22:12:47 +0800

    Update openblas_build_and_install.sh

--- openblas_build/openblas_build_and_install.sh
+++ openblas_build/openblas_build_and_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download OpenBLAS0.3.17
-wget http://www.zifuture.com:8090/upload/2021/08/OpenBLAS-0.3.17-01ef2b0812104f3bb6452c1746234e8d.tar.gz --output-document=OpenBLAS-0.3.17.tar.gz
+wget http://www.zifuture.com:1556/fs/upload/2021/08/OpenBLAS-0.3.17-01ef2b0812104f3bb6452c1746234e8d.tar.gz --output-document=OpenBLAS-0.3.17.tar.gz
 rm -rf OpenBLAS-0.3.17
 tar -zxf OpenBLAS-0.3.17.tar.gz
 cd OpenBLAS-0.3.17
